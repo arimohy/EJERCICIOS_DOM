91f6e779001b8c2ff2b6111e964a971e3bf2efe9
arimohy <arimohy@gmail.com>
2021-01-28 11:20:31 -0500

    video ejercicios cuenta regresiva

---
+++ js/dom/cuenta_regresiva.js
@@ -0,0 +1,22 @@
+const d=document;
+
+export default function countdown(id,limitDate,finalMessage){
+    const $countdown=d.getElementById(id),
+    countdownDate=new Date(limitDate).getTime();
+
+    let countdownTempo =setInterval(() => {
+        let now =new Date().getTime(),
+        limitTime=countdownDate-now,
+        days=Math.floor(limitTime/(1000*60*60*24)),
+        hours=("0"+Math.floor((limitTime % (1000*60*60*24))/(1000*60*60))).slice(-2),
+        minutes=("0"+Math.floor((limitTime % (1000*60*60))/(1000*60))).slice(-2),
+        second=("0"+Math.floor((limitTime % (1000*60))/(1000))).slice(-2);
+        $countdown.innerHTML=`<h3> Faltan ${days} dias ${hours} horas ${minutes} minutes ${second} segundos </h3>`;
+        console.log(countdownDate,now,limitTime);
+        //console.log(document.getElementById(id),$countdown)
+        if(limitTime<0){
+            clearTimeout(countdownTempo);
+            $countdown.innerHTML=`<h3>${finalMessage}</h3>`
+        }
+    },1000);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,12 +1,16 @@
 import hamburgerMenu from "./dom/1_menu_hamburguesa.js";
+import countdown from "./dom/cuenta_regresiva.js";
 import{alarm, digitalClock} from "./dom/reloj.js";
 import {moveBall,shorcut} from "./dom/teclado.js"
 const d=document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn",".panel","menu a");
-    digitalClock("#reloj","#activar-reloj","#desactivar-reloj")
-    alarm("assets/alarma_2.mp3","#activar-alarma","#desactivar-alarma")
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    alarm("assets/alarma_2.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown",
+    "March 14, 2021 00:00:00",
+    "Feliz Cumpleaños Yhomira");
 });
 
 d.addEventListener("keydown",e=>{
