da8b85bb9454be7ae1d8aec4dc3da3a3ca8a1157
arimohy <arimohy@gmail.com>
2021-02-01 15:07:52 -0500

    slides

--- js/index_dom.js
+++ js/index_dom.js
@@ -12,6 +12,7 @@ import {moveBall,shorcut} from "./dom/teclado.js"
 import darkTheme from "./dom/tema_oscuro.js";
 import searchFilters from "./dom/filtro_busquedas.js"
 import draw from "./dom/sorteo.js";
+import slider from "./dom/carrusel.js";
 const d=document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{
@@ -41,7 +42,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     getGeolocation("geolocation");
     searchFilters(".card-filter",".card");
     draw("#winner-btn",".player");
-    
+    slider();
     
 });
 
