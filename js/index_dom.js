955d6be34a4a744099ef7d3c9137585cdbae74c1
arimohy <arimohy@gmail.com>
2021-02-04 15:59:46 -0500

    narrador

--- js/index_dom.js
+++ js/index_dom.js
@@ -16,6 +16,7 @@ import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
 import contactFormValidations from "./dom/validaciones_formulario.js";
+import speechReader from "./dom/narrador.js";
 const d=document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{
@@ -59,3 +60,4 @@ d.addEventListener("keydown",e=>{
 darkTheme(".dark-theme-btn","dark-mode")
 
 networkStatus();
+speechReader();
